a74a4d4778a2030729b3dd00f6d0643022dbada6
tharindu97 <tharindukavishna@gmail.com>
2019-12-15 21:48:16 +0530

    2019-12-14-step4

---
+++ step3 - Copy - Copy/lib/login_signup_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginSignupPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => new _LoginSignupPageState();
+
+}
+
+class _LoginSignupPageState extends State<LoginSignupPage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Flutter login demo"),
+      ),
+      body: Container(
+        
+      )
+    );
+  }
+}
